b6b2d78e781ec17136f1b42bcb672adca3dc318f
Baptiste Prevost <baptiste.prevost@epfedu.fr>
2024-03-18 20:13:11 +0100

    Création d'un servlet pour la suppression d'une réservation

--- src/main/java/com/epf/rentmanager/dao/ReservationDao.java
+++ src/main/java/com/epf/rentmanager/dao/ReservationDao.java
@@ -63,10 +63,9 @@ public class ReservationDao {
         try (Connection connection = ConnectionManager.getConnection();
              PreparedStatement ps = connection.prepareStatement(DELETE_RESERVATION_QUERY)) {
             ps.setLong(1, reservation.id());
-            ps.executeUpdate();
-            ResultSet resultSet = ps.getResultSet();
-            if (resultSet.next()) {
-                return resultSet.getInt(1);
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows > 0) {
+                return affectedRows;
             } else {
                 throw new DaoException();
             }
